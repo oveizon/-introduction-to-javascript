c3e81d858e4948d99e914550a9566f231280ebc6
Wilson <oveizon@gmail.com>
2022-06-22 21:58:25 +0100

    commit of a basic javascript code that introduces me

---
+++ intro.js
@@ -0,0 +1,8 @@
+let myName = 'My name is Wilson Ovedje.';
+console.log(myName);
+
+let myHeight = 'I am 5 feet 7 inches tall.';
+console.log(myHeight);
+
+let myCountry = "I am from Nigeria.";
+console.log(myCountry);
